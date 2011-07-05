5a377fd15fe0ceb7f806027d9b84a623459ab654
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2011-07-05 10:40:28 +0200

    typso
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/applets/gnome-shell/extension.js
+++ src/applets/gnome-shell/extension.js
@@ -84,7 +84,7 @@ Indicator.prototype = {
         this._killSwitch = new PopupMenu.PopupSwitchMenuItem(_("Track clipboard changes"), true);
         this._killSwitch.connect('toggled', Lang.bind(this, this._toggleDaemon));
         this._proxy = new GPasteProxy(DBus.session, BUS_NAME, OBJECT_PATH);
-        this._proxy.launchRemote();
+        this._proxy.LaunchRemote();
         this._proxy.connect('Changed', Lang.bind(this, this._fillHistory));
         this._proxy.connect('Start', Lang.bind(this, this._started));
         this._proxy.connect('Exit', Lang.bind(this, this._exited));
