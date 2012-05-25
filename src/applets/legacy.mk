c76ffbef00d8b1229acaf54a60ad885987d396f8
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2012-05-25 20:06:49 +0200

    legacy-applet: port to libgpaste-settings
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/applets/legacy.mk
+++ src/applets/legacy.mk
@@ -40,8 +40,9 @@ bin_gpaste_applet_CFLAGS = \
 	$(NULL)
 
 bin_gpaste_applet_LDADD = \
-	$(libgpaste_core_la_file) \
+	$(libgpaste_settings_la_file) \
 	$(libgpaste_client_la_file) \
+	$(GTK_LIBS) \
 	$(NULL)
 
 CLEANFILES += \
