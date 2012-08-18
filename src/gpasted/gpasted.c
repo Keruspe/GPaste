00b9bbf6e965ee87ff7187dac8e7df43adcb6616
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2012-08-18 17:43:43 +0200

    gpasted: port to new keybindings
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/gpasted/gpasted.c
+++ src/gpasted/gpasted.c
@@ -17,14 +17,12 @@
  *      along with GPaste.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "gdbus-defines.h"
-#include "gpaste-clipboard-common.h"
-#include "gpaste-daemon.h"
-#include "gpaste-settings-keys.h"
+#include "gpaste-paste-and-pop-keybinding.h"
+#include "gpaste-show-history-keybinding.h"
 
-#include <glib/gi18n-lib.h>
-#include <xcb/xtest.h>
 #include <gpaste.h>
+#include <gpaste-daemon.h>
+#include <glib/gi18n-lib.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -67,89 +65,6 @@ reexec (GPasteDaemon *g_paste_daemon G_GNUC_UNUSED,
     execl (PKGLIBEXECDIR "/gpasted", "gpasted", NULL);
 }
 
-typedef struct {
-    GPasteXcbWrapper *xcb_wrapper;
-    GPasteHistory    *history;
-} PasteAndPopData;
-
-static void
-fake_keyboard (GPasteXcbWrapper *xcb_wrapper,
-               xcb_connection_t *connection,
-               xcb_screen_t     *screen,
-               xcb_keysym_t      keysym,
-               guint8            delay,
-               guint8            event)
-{
-    xcb_keycode_t *keycode = (xcb_keycode_t *) xcb_key_symbols_get_keycode ((xcb_key_symbols_t *) g_paste_xcb_wrapper_get_keysyms (xcb_wrapper), keysym);
-
-    if (!keycode)
-        return;
-
-    xcb_test_fake_input (connection,
-                         event,
-                         *keycode,
-                         delay,
-                         screen->root,
-                         0, 0, 0);
-
-    xcb_flush (connection);
-}
-
-static void
-paste_and_pop_get_clipboard_data (GtkClipboard     *clipboard,
-                                  GtkSelectionData *selection_data,
-                                  guint             info,
-                                  gpointer          user_data_or_owner)
-{
-    PasteAndPopData *data = (PasteAndPopData *) user_data_or_owner;
-    GPasteHistory *history = data->history;
-
-    g_paste_clipboard_get_clipboard_data (clipboard,
-                                          selection_data,
-                                          info,
-                                          G_OBJECT (g_paste_history_get (history, 0)));
-    g_paste_history_remove (history, 0);
-}
-
-static void
-paste_and_pop_clear_clipboard_data (GtkClipboard *clipboard G_GNUC_UNUSED,
-                                    gpointer      user_data_or_owner G_GNUC_UNUSED)
-{
-}
-
-static void
-paste_and_pop (PasteAndPopData *data)
-{
-    GPasteXcbWrapper *xcb_wrapper = data->xcb_wrapper;
-    xcb_connection_t *connection = (xcb_connection_t *) g_paste_xcb_wrapper_get_connection (xcb_wrapper);
-    xcb_screen_t *screen = (xcb_screen_t *) g_paste_xcb_wrapper_get_screen (xcb_wrapper);
-
-    g_return_if_fail (screen); /* This should never happen */
-
-    GtkTargetList *target_list = gtk_target_list_new (NULL, 0);
-
-    gtk_target_list_add_text_targets (target_list, 0);
-
-    gint n_targets;
-    GtkTargetEntry *targets = gtk_target_table_new_from_list (target_list, &n_targets);
-    GtkClipboard *clipboard = gtk_clipboard_get (GDK_SELECTION_PRIMARY);
-
-    gtk_clipboard_set_with_data (clipboard,
-                                 targets,
-                                 n_targets,
-                                 paste_and_pop_get_clipboard_data,
-                                 paste_and_pop_clear_clipboard_data,
-                                 data);
-
-    fake_keyboard (xcb_wrapper, connection, screen, GDK_KEY_Shift_L, 0, XCB_KEY_PRESS);
-    fake_keyboard (xcb_wrapper, connection, screen, GDK_KEY_Insert, 200, XCB_KEY_PRESS);
-    fake_keyboard (xcb_wrapper, connection, screen, GDK_KEY_Shift_L, 0, XCB_KEY_RELEASE);
-    fake_keyboard (xcb_wrapper, connection, screen, GDK_KEY_Insert, 0, XCB_KEY_RELEASE);
-
-    gtk_target_table_free (targets, n_targets);
-    gtk_target_list_unref (target_list);
-}
-
 int
 main (int argc, char *argv[])
 {
@@ -169,24 +84,13 @@ main (int argc, char *argv[])
     GPasteClipboard *clipboard = g_paste_clipboard_new (GDK_SELECTION_CLIPBOARD, settings);
     GPasteClipboard *primary = g_paste_clipboard_new (GDK_SELECTION_PRIMARY, settings);
 
-    PasteAndPopData data = {
-        .xcb_wrapper = xcb_wrapper,
-        .history = history
-    };
-
     GPasteKeybinding *keybindings[] = {
-        g_paste_keybinding_new (xcb_wrapper,
-                                settings,
-                                SHOW_HISTORY_KEY,
-                                g_paste_settings_get_show_history,
-                                (GPasteKeybindingFunc) g_paste_daemon_show_history,
-                                g_paste_daemon),
-        g_paste_keybinding_new (xcb_wrapper,
-                                settings,
-                                PASTE_AND_POP_KEY,
-                                g_paste_settings_get_paste_and_pop,
-                                (GPasteKeybindingFunc) paste_and_pop,
-                                &data)
+        G_PASTE_KEYBINDING (g_paste_paste_and_pop_keybinding_new (xcb_wrapper,
+                                                                  settings,
+                                                                  history)),
+        G_PASTE_KEYBINDING (g_paste_show_history_keybinding_new (xcb_wrapper,
+                                                                 settings,
+                                                                 g_paste_daemon))
     };
 
     c_signals[C_NAME_LOST] = g_signal_connect (G_OBJECT (g_paste_daemon),
