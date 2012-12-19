343bf08bc2a51680ab70f42246c75a1a7f672a54
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2012-12-19 11:56:39 +0100

    gpaste-client: keep things in order
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- libgpaste/client/gpaste-client.h
+++ libgpaste/client/gpaste-client.h
@@ -43,27 +43,16 @@ G_PASTE_VISIBLE
 #endif
 GType g_paste_client_get_type (void);
 
+gchar   *g_paste_client_get_element                (GPasteClient *self,
+                                                    guint32       index,
+                                                    GError      **error);
 gchar  **g_paste_client_get_history                (GPasteClient *self,
-                                                    GError      **error);
-void     g_paste_client_backup_history             (GPasteClient *self,
-                                                    const gchar  *name,
-                                                    GError      **error);
-void     g_paste_client_switch_history             (GPasteClient *self,
-                                                    const gchar  *name,
-                                                    GError      **error);
-void     g_paste_client_delete_history             (GPasteClient *self,
-                                                    const gchar  *name,
-                                                    GError      **error);
-gchar  **g_paste_client_list_histories             (GPasteClient *self,
                                                     GError      **error);
 void     g_paste_client_add                        (GPasteClient *self,
                                                     const gchar  *text,
                                                     GError      **error);
 void     g_paste_client_add_file                   (GPasteClient *self,
                                                     const gchar  *file,
-                                                    GError      **error);
-gchar   *g_paste_client_get_element                (GPasteClient *self,
-                                                    guint32       index,
                                                     GError      **error);
 void     g_paste_client_select                     (GPasteClient *self,
                                                     guint32       index,
@@ -80,6 +69,17 @@ void     g_paste_client_on_extension_state_changed (GPasteClient *self,
                                                     gboolean      state,
                                                     GError      **error);
 void     g_paste_client_reexecute                  (GPasteClient *self,
+                                                    GError      **error);
+void     g_paste_client_backup_history             (GPasteClient *self,
+                                                    const gchar  *name,
+                                                    GError      **error);
+void     g_paste_client_switch_history             (GPasteClient *self,
+                                                    const gchar  *name,
+                                                    GError      **error);
+void     g_paste_client_delete_history             (GPasteClient *self,
+                                                    const gchar  *name,
+                                                    GError      **error);
+gchar  **g_paste_client_list_histories             (GPasteClient *self,
                                                     GError      **error);
 gboolean g_paste_client_is_active                  (GPasteClient *self);
 
