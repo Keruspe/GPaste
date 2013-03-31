a4b640f3d2437c7e3ee1a0b92759c089e46990fd
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2013-03-31 20:13:25 +0200

    add gnome-control-center support
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ data/control-center.mk
@@ -0,0 +1,27 @@
+# This file is part of GPaste.
+#
+# Copyright 2013 Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
+#
+# GPaste is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GPaste is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GPaste.  If not, see <http://www.gnu.org/licenses/>.
+
+controlcenterdir = @GNOME_KEYBINDINGS_KEYSDIR@
+
+nodist_controlcenter_DATA = \
+	data/control-center/42-gpaste.xml \
+	$(NULL)	
+
+EXTRA_DIST += \
+	$(nodist_controlcenter_DATA:.xml=.xml.in) \
+	$(NULL)
+
