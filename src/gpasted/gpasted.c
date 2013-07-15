6e00c3760eede19bdedc749e03584e32c2e75e0c
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2013-07-15 18:21:23 +0200

    gnome-shell: 3.9.4
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/gpasted/gpasted.c
+++ src/gpasted/gpasted.c
@@ -60,7 +60,7 @@ reexec (GPasteDaemon *g_paste_daemon G_GNUC_UNUSED,
         gpointer      user_data G_GNUC_UNUSED)
 {
     g_main_loop_quit (main_loop);
-    execl (PKGLIBEXECDIR "/gpasted", "gpasted", NULL);
+    //execl (PKGLIBEXECDIR "/gpasted", "gpasted", NULL);
 }
 
 int
