8e2c40a2db3d832c3daa9cf88f9b34eed7a3daab
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2013-09-30 10:40:28 +0900

    settings: add About dialog
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/gpaste-settings/gpaste-settings.c
+++ src/gpaste-settings/gpaste-settings.c
@@ -21,6 +21,50 @@
 
 #include <glib/gi18n.h>
 
+#define LICENSE                                                            \
+    "GPaste is free software: you can redistribute it and/or modify"       \
+    "it under the terms of the GNU General Public License as published by" \
+    "the Free Software Foundation, either version 3 of the License, or"    \
+    "(at your option) any later version.\n\n"                              \
+    "GPaste is distributed in the hope that it will be useful,"            \
+    "but WITHOUT ANY WARRANTY; without even the implied warranty of"       \
+    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"        \
+    "GNU General Public License for more details.\n\n"                     \
+    "You should have received a copy of the GNU General Public License"    \
+    "along with GPaste.  If not, see <http://www.gnu.org/licenses/>."
+
+static void
+about_activated (GSimpleAction *action    G_GNUC_UNUSED,
+                 GVariant      *parameter G_GNUC_UNUSED,
+                 gpointer       user_data G_GNUC_UNUSED)
+{
+    const char *_authors[] = {
+        "Marc-Antoine Perennou <Marc-Antoine@Perennou.com>",
+        NULL
+    };
+    GStrv authors = g_boxed_copy (G_TYPE_STRV, _authors);
+    gtk_show_about_dialog (NULL,
+                           "program-name",   PACKAGE_NAME,
+                           "version",        PACKAGE_VERSION,
+                           "logo-icon-name", "gtk-paste",
+                           "license",        LICENSE,
+                           "authors",        authors,
+                           "copyright",      "Copyright © 2010-2013 Marc-Antoine Perennou",
+                           "comments",       "Clipboard management system",
+                           "website",        "http://www.imagination-land.org/tags/GPaste.html",
+                           "website-label",  "Follow GPaste actuality",
+                           "wrap-license",   TRUE,
+                           NULL);
+}
+
+static void
+quit_activated (GSimpleAction *action    G_GNUC_UNUSED,
+                 GVariant      *parameter G_GNUC_UNUSED,
+                 gpointer       user_data G_GNUC_UNUSED)
+{
+    g_application_quit (user_data);
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -44,6 +88,17 @@ main (int argc, char *argv[])
         g_error_free (error);
         return 1;
     }
+
+    GActionEntry app_entries[] = {
+        { "about", about_activated, NULL, NULL, NULL, { 0 } },
+        { "quit",  quit_activated,  NULL, NULL, NULL, { 0 } }
+    };
+    g_action_map_add_action_entries (G_ACTION_MAP (app), app_entries, G_N_ELEMENTS (app_entries), app);
+
+    GMenu *menu = g_menu_new ();
+    g_menu_append (menu, "About GPaste", "app.about");
+    g_menu_append (menu, "Quit", "app.quit");
+    gtk_application_set_app_menu (app, G_MENU_MODEL (menu));
 
     GPasteSettingsUiStack *stack = g_paste_settings_ui_stack_new ();
     g_paste_settings_ui_stack_fill (stack);
