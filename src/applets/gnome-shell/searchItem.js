37e029397f382171db4bd71358ce7c157f8a37bb
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2014-04-15 19:20:42 +0900

    gnome-shell: add delete icon
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/applets/gnome-shell/searchItem.js
+++ src/applets/gnome-shell/searchItem.js
@@ -22,6 +22,7 @@ const Lang = imports.lang;
 
 const PopupMenu = imports.ui.popupMenu;
 
+const Clutter = imports.gi.Clutter
 const St = imports.gi.St;
 
 const GPasteSearchItem = new Lang.Class({
@@ -48,6 +49,11 @@ const GPasteSearchItem = new Lang.Class({
             icon_name:'edit-find-symbolic'
         }));
         this._entry.clutter_text.connect('text-changed', Lang.bind(this, this._onTextChanged));
+
+        this._clearIcon = new St.Icon({
+            style_class: 'search-entry-icon',
+            icon_name: (this._entry.get_text_direction() == Clutter.TextDirection.RTL) ? 'edit-clear-rtl-symbolic' : 'edit-clear-symbolic'
+        });
     },
 
     get text() {
@@ -59,6 +65,18 @@ const GPasteSearchItem = new Lang.Class({
     },
 
     _onTextChanged: function(se, prop) {
+        let dummy = (this.text.length == 0);
+        this._entry.set_secondary_icon((dummy) ? null : this._clearIcon);
+        if (!dummy && this._iconClickedId == 0) {
+            this._iconClickedId = this._entry.connect('secondary-icon-clicked', Lang.bind(this, this._reset));
+        }
         this.emit('text-changed');
+    },
+
+    _reset: function() {
+        this._entry.text = '';
+        let text = this._entry.clutter_text;
+        text.set_cursor_visible(true);
+        text.set_selection(0, 0);
     }
 });
