cf329ba1247da29a1816493f5073a506b5b27925
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2014-11-10 18:46:24 +0100

    switch to appstream
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- data/appstream.mk
+++ data/appstream.mk
@@ -15,31 +15,31 @@
 # You should have received a copy of the GNU General Public License
 # along with GPaste.  If not, see <http://www.gnu.org/licenses/>.
 
-@APPDATA_XML_RULES@
+@APPSTREAM_XML_RULES@
 @INTLTOOL_XML_RULE@
 
-appdata_in_files = \
-	data/appdata/org.gnome.GPaste.Settings.appdata.xml.in \
+appstream_in_files = \
+	data/appstream/org.gnome.GPaste.Settings.appdata.xml.in \
 	$(NULL)
 
 if ENABLE_APPLET
-appdata_in_files += \
-	data/appdata/org.gnome.GPaste.Applet.appdata.xml.in \
+appstream_in_files += \
+	data/appstream/org.gnome.GPaste.Applet.appdata.xml.in \
 	$(NULL)
 endif
 
 if ENABLE_UNITY
-appdata_in_files += \
-	data/appdata/org.gnome.GPaste.AppIndicator.appdata.xml.in \
+appstream_in_files += \
+	data/appstream/org.gnome.GPaste.AppIndicator.appdata.xml.in \
 	$(NULL)
 endif
 
-appdata_XML = $(appdata_in_files:.xml.in=.xml)
+appstream_XML = $(appstream_in_files:.xml.in=.xml)
 
 EXTRA_DIST += \
-	$(appdata_in_files) \
+	$(appstream_in_files) \
 	$(NULL)
 
 CLEANFILES += \
-	$(appdata_XML) \
+	$(appstream_XML) \
 	$(NULL)
