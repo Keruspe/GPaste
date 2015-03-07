a7a4e093023ebc85a10682ee8d2ed1b79ad56459
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2015-03-07 13:04:04 +0100

    gpaste.h: reorder
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/libgpaste/gpaste.h
+++ src/libgpaste/gpaste.h
@@ -22,9 +22,56 @@
 
 #define __G_PASTE_H_INSIDE__
 
+/* Misc. macros */
 #include <gpaste-macros.h>
+
+/* Core GPaste Components */
+#include <gpaste-clipboard.h>
+#include <gpaste-clipboards-manager.h>
+#include <gpaste-history.h>
+#include <gpaste-image-item.h>
+#include <gpaste-item.h>
+#include <gpaste-password-item.h>
+#include <gpaste-text-item.h>
+#include <gpaste-uris-item.h>
+
+/* GPasteUtil */
 #include <gpaste-util.h>
 
+/* GPasteKeybinder */
+#include <gpaste-keybinder.h>
+#include <gpaste-keybinding.h>
+#include <gpaste-make-password-keybinding.h>
+#include <gpaste-pop-keybinding.h>
+#include <gpaste-show-history-keybinding.h>
+#include <gpaste-sync-clipboard-to-primary-keybinding.h>
+#include <gpaste-sync-primary-to-clipboard-keybinding.h>
+#include <gpaste-upload-keybinding.h>
+
+/* GPasteDaemon */
+#include <gpaste-daemon.h>
+#include <gpaste-gdbus-defines.h>
+#include <gpaste-update-enums.h>
+
+/* GPasteClient */
+#include <gpaste-client.h>
+
+/* GPasteGnomeShellClient */
+#include <gpaste-gnome-shell-client.h>
+
+/* GPasteScreensaverClient */
+#include <gpaste-screensaver-client.h>
+
+/* GPasteSettings */
+#include <gpaste-gsettings-keys.h>
+#include <gpaste-settings.h>
+
+/* GPasteSettingsUi */
+#include <gpaste-settings-ui-panel.h>
+#include <gpaste-settings-ui-stack.h>
+#include <gpaste-settings-ui-widget.h>
+
+/* GPasteApplet */
 #include <gpaste-applet-about.h>
 #include <gpaste-applet-icon.h>
 #include <gpaste-applet-menu.h>
@@ -33,30 +80,8 @@
 #include <gpaste-applet-status-icon.h>
 #include <gpaste-applet-ui.h>
 #include <gpaste-applet.h>
-#include <gpaste-client.h>
-#include <gpaste-clipboard.h>
-#include <gpaste-clipboards-manager.h>
-#include <gpaste-daemon.h>
-#include <gpaste-gdbus-defines.h>
-#include <gpaste-gnome-shell-client.h>
-#include <gpaste-gsettings-keys.h>
-#include <gpaste-history.h>
-#include <gpaste-image-item.h>
-#include <gpaste-item.h>
-#include <gpaste-keybinder.h>
-#include <gpaste-keybinding.h>
-#include <gpaste-make-password-keybinding.h>
-#include <gpaste-password-item.h>
-#include <gpaste-pop-keybinding.h>
-#include <gpaste-screensaver-client.h>
-#include <gpaste-settings-ui-panel.h>
-#include <gpaste-settings-ui-stack.h>
-#include <gpaste-settings-ui-widget.h>
-#include <gpaste-settings.h>
-#include <gpaste-show-history-keybinding.h>
-#include <gpaste-sync-clipboard-to-primary-keybinding.h>
-#include <gpaste-sync-primary-to-clipboard-keybinding.h>
-#include <gpaste-text-item.h>
+
+/* GPasteUi */
 #include <gpaste-ui-about.h>
 #include <gpaste-ui-delete.h>
 #include <gpaste-ui-empty.h>
@@ -67,9 +92,6 @@
 #include <gpaste-ui-settings.h>
 #include <gpaste-ui-switch.h>
 #include <gpaste-ui-window.h>
-#include <gpaste-update-enums.h>
-#include <gpaste-upload-keybinding.h>
-#include <gpaste-uris-item.h>
 
 #undef __G_PASTE_H_INSIDE__
 
