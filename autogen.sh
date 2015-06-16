b27286854d6f5b7c9d585fcd40acf589ca7ee9e6
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2015-06-16 13:53:36 +0200

    autogen: add scan-build
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
+scan_build() {
+    scan-build --use-analyzer=/usr/bin/clang "${@}"
+}
+
 run_action() {
     local action="${1}"
     shift
 
+    local configure_args=(
+        --prefix=/usr
+        --sysconfdir=/etc
+        --enable-applet
+        --enable-appstream-util
+        --enable-gnome-shell-extension
+        --enable-introspection
+        --enable-unity
+        --enable-vala
+        --enable-x-keybinder
+    )
+
     case "${action}" in
         configure-full)
-            ./configure --prefix=/usr --sysconfdir=/etc --enable-x-keybinder --enable-gnome-shell-extension --enable-applet --enable-appstream-util --enable-unity --enable-vala --enable-introspection "${@}"
+            ./configure "${configure_args[@]}" "${@}"
+            ;;
+        scan-build)
+            scan_build ./configure "${configure_args[@]}" "${@}"
+            scan_build make
             ;;
     esac
 }
