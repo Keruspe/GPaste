a0f6445ff51ea4ea31e387ba46f013c8c7b2af6e
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2015-06-16 13:41:52 +0200

    autogen.sh: rework, add "full"
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,21 @@
-#!/bin/sh
-autoreconf -i -Wall
-intltoolize --force --automake
+#!/bin/bash
 
+run_action() {
+    local action="${1}"
+    shift
+
+    case "${action}" in
+        configure-full)
+            ./configure --prefix=/usr --sysconfdir=/etc --enable-x-keybinder --enable-gnome-shell-extension --enable-applet --enable-appstream-util --enable-unity --enable-vala --enable-introspection "${@}"
+            ;;
+    esac
+}
+
+main() {
+    autoreconf -i -Wall
+    intltoolize --force --automake
+
+    [[ "#{#}" != 0 ]] && run_action "${@}"
+}
+
+main "${@}"
