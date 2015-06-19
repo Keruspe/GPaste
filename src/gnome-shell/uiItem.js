4c067570e3517f9a858ebaf74d3e9411fa77f2b2
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2015-06-19 18:07:56 +0200

    gnome-shell: don't make UiItem extend St.Button
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/gnome-shell/uiItem.js
+++ src/gnome-shell/uiItem.js
@@ -26,19 +26,18 @@ const St = imports.gi.St;
 
 const GPasteUiItem = new Lang.Class({
     Name: 'GPasteUiItem',
-    Extends: St.Button,
 
     _init: function(menu) {
-        this.parent({
+        this.actor = new St.Button({
             reactive: true,
             can_focus: true,
             track_hover: true,
             style_class: 'system-menu-action'
         });
 
-        this.child = new St.Icon({ icon_name: 'go-home-symbolic' });
+        this.actor.child = new St.Icon({ icon_name: 'go-home-symbolic' });
 
-        this.connect('clicked', function() {
+        this.actor.connect('clicked', function() {
             menu.itemActivated();
             GPaste.util_spawn('Ui');
         });
