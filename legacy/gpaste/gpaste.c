71ec29088bdecaf9a07badda829b98c846645681
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2015-09-15 22:18:26 +0200

    gpaste: add legacy compat wrapper
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ legacy/gpaste/gpaste.c
@@ -0,0 +1,36 @@
+/*
+ *      This file is part of GPaste.
+ *
+ *      Copyright 2015 Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
+ *
+ *      GPaste is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      GPaste is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with GPaste.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int
+main (int   argc __attribute__((unused)),
+      char *argv[])
+{
+    const char *gpastectl = BINDIR "/gpastectl";
+
+    fprintf (stderr, "The \"gpaste\" command is deprecatedi and will soon be removed, please use \"gpastectl\" instead.");
+
+    argv[0] = (char *) gpastectl;
+    execv (gpastectl, argv);
+
+    return EXIT_SUCCESS;
+}
