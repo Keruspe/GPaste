27fae6b3d56eee5a809255b2d72f34feb98923f0
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2016-01-18 21:44:51 +0100

    add systemd user units
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ data/systemd.mk
@@ -0,0 +1,48 @@
+## This file is part of GPaste.
+##
+## Copyright 2016 Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
+##
+## GPaste is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## GPaste is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with GPaste.  If not, see <http://www.gnu.org/licenses/>.
+
+nodist_systemduserunit_DATA =                   \
+	%D%/systemd/org.gnome.GPaste.service    \
+	%D%/systemd/org.gnome.GPaste.Ui.service \
+	$(NULL)
+
+if ENABLE_APPLET
+nodist_systemduserunit_DATA +=                      \
+	%D%/systemd/org.gnome.GPaste.Applet.service \
+	$(NULL)
+endif
+
+if ENABLE_UNITY
+nodist_systemduserunit_DATA +=                            \
+	%D%/systemd/org.gnome.GPaste.AppIndicator.service \
+	$(NULL)
+endif
+
+SUFFIXES += .service .systemd.in
+.systemd.in.service:
+	@ $(MKDIR_P) $(@D)
+	$(AM_V_GEN) $(SED)                                \
+	    -e 's,[@]pkglibexecdir[@],$(pkglibexecdir),g' \
+	    <$< >$@
+
+EXTRA_DIST +=                                               \
+	$(nodist_systemduserunit_DATA:.service=.systemd.in) \
+	$(NULL)
+
+CLEANFILES +=                          \
+	$(nodist_systemduserunit_DATA) \
+	$(NULL)
