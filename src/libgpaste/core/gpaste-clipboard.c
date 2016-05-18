cdcb2e94195c0dfeb47376b33ac6446b4fd9d0b3
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2016-05-18 22:45:03 +0200

    clipboard: avoid calling gtk_clipboard_store too often
    Avoid hitting some racy code in gtk+
    
    Probably fixes #151
    
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/libgpaste/core/gpaste-clipboard.c
+++ src/libgpaste/core/gpaste-clipboard.c
@@ -29,6 +29,13 @@ struct _GPasteClipboard
     GObject parent_instance;
 };
 
+typedef enum
+{
+    CLIP_STATE_IDLE,
+    CLIP_STATE_WAITING,
+    CLIP_STATE_BUSY
+} ClipState;
+
 typedef struct
 {
     GdkAtom         target;
@@ -38,6 +45,11 @@ typedef struct
     gchar          *image_checksum;
 
     gulong          owner_change_signal;
+
+    /* GtkClipboard has a bug where it overrides an internal GMainLoop
+     * if we call gtk_clipboard_store twice in less than 10s, resulting
+     * in a g_main_loop_quit (NULL) call making GPaste crash */
+    ClipState       state;
 } GPasteClipboardPrivate;
 
 G_DEFINE_TYPE_WITH_PRIVATE (GPasteClipboard, g_paste_clipboard, G_TYPE_OBJECT)
@@ -267,6 +279,43 @@ g_paste_clipboard_set_text (GPasteClipboard            *self,
                                 data);
 }
 
+static void g_paste_clipboard_private_store (GPasteClipboardPrivate *priv);
+
+static gboolean
+g_paste_clipboard_private_store_timeout (gpointer user_data)
+{
+    GPasteClipboardPrivate *priv = user_data;
+    gboolean waiting = (priv->state == CLIP_STATE_WAITING);
+
+    /* Reset us as IDLE */
+    priv->state = CLIP_STATE_IDLE;
+
+    /* If we were waiting, perform the store now */
+    if (waiting)
+        g_paste_clipboard_private_store (priv);
+}
+
+static void
+g_paste_clipboard_private_store (GPasteClipboardPrivate *priv)
+{
+    switch (priv->state)
+    {
+    case CLIP_STATE_IDLE:
+        /* We're idle, store right now and mark us BUSY for 10 secs */
+        gtk_clipboard_store (priv->real);
+        priv->state = CLIP_STATE_BUSY;
+        g_timeout_add_seconds (10, g_paste_clipboard_private_store_timeout, priv);
+        break;
+    case CLIP_STATE_BUSY:
+        /* We're busy, wait until we're finished to store once more */
+        priv->state = CLIP_STATE_WAITING;
+        break;
+    case CLIP_STATE_WAITING:
+        /* We're already waiting, nothing to do */
+        break;
+    }
+}
+
 /**
  * g_paste_clipboard_select_text:
  * @self: a #GPasteClipboard instance
@@ -285,12 +334,11 @@ g_paste_clipboard_select_text (GPasteClipboard *self,
     g_return_if_fail (g_utf8_validate (text, -1, NULL));
 
     GPasteClipboardPrivate *priv = g_paste_clipboard_get_instance_private (self);
-    GtkClipboard *real = priv->real;
 
     /* Avoid cycling twice as gtk_clipboard_set_text will make the clipboards manager react */
     g_paste_clipboard_private_set_text (priv, text);
-    gtk_clipboard_set_text (real, text, -1);
-    gtk_clipboard_store (real);
+    gtk_clipboard_set_text (priv->real, text, -1);
+    g_paste_clipboard_private_store (priv);
 }
 
 static void
@@ -369,7 +417,7 @@ g_paste_clipboard_private_select_uris (GPasteClipboardPrivate *priv,
                                   g_paste_clipboard_get_clipboard_data,
                                   g_paste_clipboard_clear_clipboard_data,
                                   g_object_ref (item));
-    gtk_clipboard_store (real);
+    g_paste_clipboard_private_store (priv);
 
     gtk_target_table_free (targets, n_targets);
 }
@@ -694,6 +742,8 @@ g_paste_clipboard_init (GPasteClipboard *self)
 
     priv->text = NULL;
     priv->image_checksum = NULL;
+
+    priv->state = CLIP_STATE_IDLE;
 }
 
 /**
