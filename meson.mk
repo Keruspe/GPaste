b288cc86c7e270ab5757b6cd2c1e1a4f1100e0a1
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-03-17 14:28:41 +0100

    autotools: add meson to dist
    Fixes #266
    
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ meson.mk
@@ -0,0 +1,28 @@
+## This file is part of GPaste.
+##
+## Copyright (c) 2010-2019, Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
+
+EXTRA_DIST +=                                \
+	meson.build                          \
+	meson_options.txt                    \
+	data/dbus/meson.build                \
+	data/desktop/meson.build             \
+	data/control-center/meson.build      \
+	data/appstream/meson.build           \
+	data/completions/meson.build         \
+	data/search-provider/meson.build     \
+	data/systemd/meson.build             \
+	data/gsettings/meson.build           \
+	data/meson.build                     \
+	man/1/meson.build                    \
+	man/meson.build                      \
+	po/meson.build                       \
+	src/daemon/meson.build               \
+	src/gnome-shell/meson.build          \
+	src/libgpaste/meson.build            \
+	src/ui/meson.build                   \
+	src/client/meson.build               \
+	src/meson.build                      \
+	tests/gnome-shell-client/meson.build \
+	tests/meson.build                    \
+	$(NULL)
