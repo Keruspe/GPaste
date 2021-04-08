45a825d4a3aba8152d7d281b40b5d3c8d8280dd7
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2021-04-08 08:53:02 +0200

    gnome-shell: placeholder prefs
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/gnome-shell/prefs.js
+++ src/gnome-shell/prefs.js
@@ -4,11 +4,13 @@
  * Copyright (c) 2010-2019, Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
  */
 
+imports.gi.versions.Gtk = '4.0';
+
 const Config = imports.misc.config;
 
 const Gettext = imports.gettext;
 
-const { GPaste } = imports.gi;
+//const { GPaste } = imports.gi;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
@@ -19,10 +21,19 @@ function init() {
     Gettext.textdomain(domain);
 }
 
+function GPaste40PlaceHolder() {
+    const { GLib, Gtk } = imports.gi;
+    GLib.spawn_async(null, ["gpaste-client", "settings"], null, GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD, null);
+    return new Gtk.Label({ label: "Opened the GPaste settings." });
+}
+
 function buildPrefsWidget() {
+    /*
     let widget = new GPaste.SettingsUiWidget({ margin: 12 });
     if (widget) {
         widget.show_all();
     }
+    */
+    let widget = GPaste40PlaceHolder();
     return widget;
 }
