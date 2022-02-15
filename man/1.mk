67b779b9a1c9385aadc5777ca694e3f305accfa8
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2022-02-15 15:31:40 +0100

    drop autotools
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- man/1.mk
+++
@@ -1,7 +0,0 @@
-## This file is part of GPaste.
-##
-## Copyright (c) 2010-2018, Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
-
-dist_man_MANS =               \
-	%D%/1/gpaste-client.1 \
-	$(NULL)
