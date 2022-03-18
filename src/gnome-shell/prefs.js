e285a8ae3b36687a554ddc109b7ff940748b1798
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2022-03-18 14:38:37 +0100

    gnome-shell: use the new prefs widget
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/gnome-shell/prefs.js
+++ src/gnome-shell/prefs.js
@@ -1,14 +1,14 @@
 /*
  * This file is part of GPaste.
  *
- * Copyright (c) 2010-2019, Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
+ * Copyright (c) 2010-2022, Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
  */
 
-imports.gi.versions.Gtk = '4.0';
+imports.gi.versions.GPasteGtk = '4';
 
 const ExtensionUtils = imports.misc.extensionUtils;
 
-//const { GPaste } = imports.gi;
+const { GPasteGtk } = imports.gi;
 
 /** */
 function init() {
@@ -16,24 +16,8 @@ function init() {
 }
 
 /**
- * @returns {Gtk.Label} - the placeholder label
- */
-function GPaste40PlaceHolder() {
-    const { GLib, Gtk } = imports.gi;
-    GLib.spawn_async(null, ["gpaste-client", "settings"], null, GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD, null);
-    return new Gtk.Label({ label: "Opened the GPaste settings." });
-}
-
-/**
  * @returns {Gtk.Widget} - the prefs widget
  */
 function buildPrefsWidget() {
-    /*
-    let widget = new GPaste.SettingsUiWidget({ margin: 12 });
-    if (widget) {
-        widget.show_all();
-    }
-    */
-    let widget = GPaste40PlaceHolder();
-    return widget;
+    return GPasteGtk.preferences_widget.new ();
 }
