fb2600cd9e383a6cd82332bbf28f25915a65e9f7
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2022-03-18 14:43:00 +0100

    gnome-shell: fix prefs
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/gnome-shell/prefs.js
+++ src/gnome-shell/prefs.js
@@ -19,5 +19,5 @@ function init() {
  * @returns {Gtk.Widget} - the prefs widget
  */
 function buildPrefsWidget() {
-    return GPasteGtk.preferences_widget.new ();
+    return GPasteGtk.PreferencesWidget.new ();
 }
