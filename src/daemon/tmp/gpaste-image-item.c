70109515d841f792747884f24d2854ef3ae2b251
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2026-05-10 22:18:54 +0200

    warn when failing to load images
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/daemon/tmp/gpaste-image-item.c
+++ src/daemon/tmp/gpaste-image-item.c
@@ -157,8 +157,10 @@ g_paste_image_item_set_state (GPasteItem     *self,
     case G_PASTE_ITEM_STATE_ACTIVE:
         if (!priv->image)
         {
-            priv->image = gdk_pixbuf_new_from_file (g_paste_item_get_value (self),
-                                                    NULL); /* Error */
+            g_autoptr (GError) error = NULL;
+            priv->image = gdk_pixbuf_new_from_file (g_paste_item_get_value (self), &error);
+            if (error)
+                g_warning ("Failed to load image from %s: %s", g_paste_item_get_value (self), error->message);
             priv->checksum = g_paste_gtk_util_compute_checksum (priv->image);
         }
         break;
