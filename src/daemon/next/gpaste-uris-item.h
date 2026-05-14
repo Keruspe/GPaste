8fc0e41bf9afec40d947bfcfe670947540ce0d9f
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2026-05-14 16:26:17 +0200

    daemon: add gtk4 version of uris item
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ src/daemon/next/gpaste-uris-item.h
@@ -0,0 +1,23 @@
+/*
+ * This file is part of GPaste.
+ *
+ * Copyright (c) 2010-2022, Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
+ */
+
+#pragma once
+
+#include <gpaste-text-item.h>
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define G_PASTE_TYPE_URIS_ITEM (g_paste_uris_item_get_type ())
+
+G_PASTE_FINAL_TYPE (UrisItem, uris_item, URIS_ITEM, GPasteTextItem)
+
+GdkFileList *g_paste_uris_item_get_file_list (const GPasteUrisItem *self);
+
+GPasteItem  *g_paste_uris_item_new (GdkFileList *file_list);
+
+G_END_DECLS
